8350ea74fcf628115f99bb5c1f2f13169efc8933
Nnnikitosss <nnnikitosss@yandex.ru>
2024-11-07 20:53:34 +0300

    web

--- Java/displayDishes.js
+++ Java/displayDishes.js
@@ -1,14 +1,80 @@
-
 document.addEventListener("DOMContentLoaded", () => {
+    const lunchCombos = [ // Возможный комбинации блюд для заказа
+        ["soup", "main", "salat", "drink"],
+        ["soup", "main", "drink"],
+        ["soup", "salat", "drink"],
+        ["main", "salat", "drink"],
+        ["main", "drink"]
+    ];
+
+    // Находим форму на странице и добавляем обработчик события на её отправку
+    const form = document.querySelector("form"); 
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();// Предотвращаем стандартное поведение отправки формы
+
+        // Проверяем состав заказа
+        if (!checkLunchComposition()) {
+            return;
+        }
+
+        form.submit();// Если проверка прошла, отправляем форму
+    });
+
+// Функция для проверки состава заказа
+    const checkLunchComposition = () => {
+        let selectedCategories = Object.keys(selectedDishes).filter(category => selectedDishes[category]); // Извлекаем все категории блюд и фильтруем только те, которые выбраны
+        let isValidCombo = lunchCombos.some(combo => combo.every(item => selectedCategories.includes(item))); // Проверяем, соответствует ли комбинация блюд одной из допустимых
+        
+        // Проверка различных условий и вывод соответствующих сообщений
+        if (selectedCategories.length === 0) {
+            showAlert("Ничего не выбрано. Выберите блюда для заказа");
+            return false;
+        } else if (selectedCategories.includes("desert") && selectedCategories.length === 1) {
+            showAlert("Выберите главное блюдо");
+            return false;
+        } else if (selectedCategories.includes("main") && selectedCategories.includes("salat") && !selectedCategories.includes("drink") && selectedCategories.includes("soup") && selectedCategories.includes("desert")) {
+            showAlert("Выберите напиток");
+            return false;
+        } else if (selectedCategories.includes("soup") && !selectedCategories.includes("main") && !selectedCategories.includes("salat")) {
+            showAlert("Выберите главное блюдо, салат или стартер");
+            return false;
+        } else if (selectedCategories.includes("salat") && !selectedCategories.includes("soup") && !selectedCategories.includes("main")) {
+            showAlert("Выберите суп или главное блюдо");
+            return false;
+        } else if (selectedCategories.includes("drink") && !selectedCategories.includes("main") && !selectedCategories.includes("salat")) {
+            showAlert("Выберите главное блюдо");
+            return false;
+        } else if (!isValidCombo) {
+            showAlert("Выберите все необходимые блюда для заказа");
+            return false;
+        }
+
+        return true;// Если все проверки прошли, возвращаем true
+    };
+
+    // Функция для создания и отображения уведомлений
+    const showAlert = (message) => { 
+        // Создаем элемент уведомления
+        const alertBox = document.createElement("div");
+        alertBox.classList.add("alert"); // Добавляем класс для стилей
+        alertBox.innerHTML = `<p>${message}</p><button>Окей 👌</button>`; // Устанавливаем текст сообщения и кнопки
+        // Добавляем уведомление на страницу
+        document.body.appendChild(alertBox);
+
+        // Добавляем обработчик для кнопки закрытия уведомления
+        const alertButton = alertBox.querySelector("button");
+        alertButton.addEventListener("click", () => {
+            alertBox.remove();
+        });
+    };
+
+
+    
     const soupSection = document.querySelector("#soup-section .dishes");
     const mainSection = document.querySelector("#main-section .dishes");
     const salatSection = document.querySelector("#salat-section .dishes");
     const drinkSection = document.querySelector("#drink-section .dishes");
-    const desertSection = document.querySelector("#desert-section .dishes");   
-
-
-
-
+    const desertSection = document.querySelector("#desert-section .dishes");
 
     document.querySelectorAll(".filters button").forEach(filter => {
         filter.addEventListener("click", (event) => {
@@ -39,22 +105,14 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     };
 
-
-
-
-
     dishes.sort((a, b) => a.name.localeCompare(b.name));
 
+    dishes.forEach(dish => {
+        const dishElement = document.createElement("div");
+        dishElement.classList.add("dish");
+        dishElement.setAttribute("data-dish", dish.keyword);
 
-
-
-    dishes.forEach(dish => { // проходим по каждому объекту в массиве
-        const dishElement = document.createElement("div");// создает новый элемент div для каждого блюда
-        dishElement.classList.add("dish");// добавляет класс dish  к новому элементу
-        dishElement.setAttribute("data-dish", dish.keyword);// устанавливает арибует data-dish
-
-        //заплняем элементы блюда информацией 
-        dishElement.innerHTML = ` 
+        dishElement.innerHTML = `
             <img src="${dish.image}" alt="${dish.name}">
             <div class="dish-info">
                 <p class="price">${dish.price}₽</p>
@@ -63,7 +121,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 <button class="add">Добавить</button>
             </div>
         `;
-// добавление элемента блюда в нужную секцию
+
         if (dish.category === "soup") {
             soupSection.appendChild(dishElement);
         } else if (dish.category === "main") {
@@ -72,21 +130,103 @@ document.addEventListener("DOMContentLoaded", () => {
             salatSection.appendChild(dishElement);
         } else if (dish.category === "drink") {
             drinkSection.appendChild(dishElement);
-        } else if (dish.category === "desert") {  
+        } else if (dish.category === "desert") {
             desertSection.appendChild(dishElement);
         }
     });
 
-    //определение элементов формы заказа
     const orderForm = {
         soup: document.getElementById("selected-soup"),
         main: document.getElementById("selected-main"),
         salat: document.getElementById("selected-salat"),
         drink: document.getElementById("selected-drink"),
-        desert: document.getElementById("selected-desert"), 
+        desert: document.getElementById("selected-desert"),
         totalPrice: document.querySelector("#total-price .price-value")
     };
-// инициалиируем выбранные блюда
+//////////////////////////////////////////////////////////////////////////////////
+// const filterDishes = (section, kind) => {
+//     const sectionDishes = section.querySelectorAll(".dish");
+//     sectionDishes.forEach(dishElement => {
+//         const dishKeyword = dishElement.getAttribute("data-dish");
+//         const dish = dishes.find(d => d.keyword === dishKeyword);
+//         if (kind === "all" || dish.kind.includes(kind)) {
+//             dishElement.style.display = "block";
+//         } else {
+//             dishElement.style.display = "none";
+//         }
+//     });
+// };
+// const loadDishes = async () => {
+//     try {
+//         console.log("Начинаем загрузку блюд...");
+//         const response = await fetch('http://lab7-api.std-900.ist.mospolytech.ru/api/dishes');
+//         console.log("Ответ от API получен");
+        
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok ' + response.statusText);
+//         }
+        
+//         const dishes = await response.json(); // Получаем блюда из API и преобразуем их в формат JSON
+//         console.log("Данные блюд загружены:", dishes);
+
+//         // Сортировка блюд по имени
+//         dishes.sort((a, b) => a.name.localeCompare(b.name));
+
+//         // Добавление блюд на страницу
+//         dishes.forEach(dish => {
+//             const dishElement = document.createElement("div");
+//             dishElement.classList.add("dish");
+//             dishElement.setAttribute("data-dish", dish.keyword);
+
+//             dishElement.innerHTML = `
+//                 <img src="${dish.image}" alt="${dish.name}">
+//                 <div class="dish-info">
+//                     <p class="price">${dish.price}₽</p>
+//                     <p class="name">${dish.name}</p>
+//                     <p class="weight">${dish.count}</p>
+//                     <button class="add">Добавить</button>
+//                 </div>
+//             `;
+
+//             // Размещение блюда в соответствующий раздел
+//             if (dish.category === "soup") {
+//                 soupSection.appendChild(dishElement);
+//             } else if (dish.category === "main") {
+//                 mainSection.appendChild(dishElement);
+//             } else if (dish.category === "salat") {
+//                 salatSection.appendChild(dishElement);
+//             } else if (dish.category === "drink") {
+//                 drinkSection.appendChild(dishElement);
+//             } else if (dish.category === "desert") {
+//                 desertSection.appendChild(dishElement);
+//             }
+//         });
+
+//     } catch (error) {
+//         console.error('Fetching dishes failed:', error);
+//     }
+// };
+
+// // Вызовите функцию loadDishes(), чтобы данные загрузились при загрузке страницы
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("DOMContentLoaded event triggered");
+//     loadDishes(); // Загружаем блюда при загрузке страницы
+//     // Ваш оставшийся код
+// });
+
+// // Инициализация формы заказа
+// const orderForm = {
+//     soup: document.getElementById("selected-soup"),
+//     main: document.getElementById("selected-main"),
+//     salat: document.getElementById("selected-salat"),
+//     drink: document.getElementById("selected-drink"),
+//     desert: document.getElementById("selected-desert"),
+//     totalPrice: document.querySelector("#total-price .price-value")
+// };
+// console.log("Элементы секций:", soupSection, mainSection, salatSection, drinkSection, desertSection);
+
+
+//////////////////////////////////////////////////////////    
     let selectedDishes = {
         soup: null,
         main: null,
@@ -95,15 +235,10 @@ document.addEventListener("DOMContentLoaded", () => {
         desert: null
     };
 
-
-
-    
-   
-// добавляем переменные
     const updateOrder = () => {
         let total = 0;
         let isAnyDishSelected = false;
-// обновляем информацию о блюдах если блюдо выбрано то показана информация если нет то показано не выбрано 
+
         for (let category in selectedDishes) {
             if (selectedDishes[category]) {
                 orderForm[category].textContent = `${selectedDishes[category].name} ${selectedDishes[category].price}₽`;
@@ -113,23 +248,24 @@ document.addEventListener("DOMContentLoaded", () => {
                 orderForm[category].textContent = "Не выбрано";
             }
         }
-// определение элементов для отображения сообщений и блоков заказа
+
+
         const noSelectionMessage = document.getElementById("no-selection-message");
         const soupOrder = document.getElementById("soup-order");
         const mainOrder = document.getElementById("main-order");
         const salatOrder = document.getElementById("salat-order");
         const drinkOrder = document.getElementById("drink-order");
-        const desertOrder = document.getElementById("desert-order");  
+        const desertOrder = document.getElementById("desert-order");
         const totalPriceBlock = document.getElementById("total-price-block");
 
-        //отображение или скрытие элементов в зависимости выбрано блюдо или нет
+
         if (!isAnyDishSelected) {
             noSelectionMessage.style.display = 'block';
             soupOrder.style.display = 'none';
             mainOrder.style.display = 'none';
             salatOrder.style.display = 'none';
             drinkOrder.style.display = 'none';
-            desertOrder.style.display = 'none';  
+            desertOrder.style.display = 'none';
             totalPriceBlock.style.display = 'none';
         } else {
             noSelectionMessage.style.display = 'none';
@@ -137,13 +273,11 @@ document.addEventListener("DOMContentLoaded", () => {
             mainOrder.style.display = 'block';
             salatOrder.style.display = 'block';
             drinkOrder.style.display = 'block';
-            desertOrder.style.display = 'block';   
+            desertOrder.style.display = 'block';
             totalPriceBlock.style.display = 'block';
             orderForm.totalPrice.textContent = `${total}₽`;
         }
     };
-
-
 
     document.querySelectorAll(".dish").forEach(dishElement => {
         dishElement.addEventListener("click", (event) => {
